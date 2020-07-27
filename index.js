e81ce4ab92fe6b9508f284df284450f7dcd26294
WahidX <wahidx93@gmail.com>
2020-07-27 12:16:20 +0530

    First part done

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// Router
 app.use('/', require('./routes'));
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 app.listen(port, function(err) {
     if(err) {
