967010ad765b2d503c18406db69414df4fc63199
WahidX <wahidx93@gmail.com>
2020-07-28 00:01:51 +0530

    Second Part done

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+const db = require('./config/mongoose');
+
+// Assets
+app.use(express.static('./assets'));
+
+// Layouts
+app.use(expressEjsLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // Router
 app.use('/', require('./routes'));
