7d1a340aa107af0c47eaaac18209b3135950cb85
wahidx <wahidx93@gmail.com>
2020-08-26 04:08:25 +0530

    api initial dirs made

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', homeController.home);
 router.use('/user', require('./users'));
 router.use('/post', require('./post'));
 router.use('/comment', require('./comment'));
+router.use('/api', require('./api'));
 
 console.log('router loaded');
 
