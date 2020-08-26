3bfb168c0af8028e5c959c96bc4fe2428d901e11
wahidx <wahidx93@gmail.com>
2020-08-27 03:16:46 +0530

    API Post destroy added

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -17,3 +17,33 @@ module.exports.index = async function(req, res){
         posts: posts
     })
 }
+
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if(post && post.user == req.user.id){
+            post.remove();
+    
+            await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                console.log('xhr Gotten');
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: 'Post deleted successfully'
+                })
+            }
+
+            req.flash('success', 'Post deleted successfully!');
+            return res.redirect('back');
+        }
+        else{
+            req.flash('error', 'You cannot delete this post!');
+        }
+    } catch(err) {
+        console.log('Err: ',err);
+    }
+}
