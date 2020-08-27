99851f9793b057633a22bff5bb3e17854e48db26
wahidx <wahidx93@gmail.com>
2020-08-28 03:14:17 +0530

    API destroy added

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,6 @@
+const passport = require('passport');
+const JWTstrategy = require("passport-jwt"),Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+const User = require('../models/users');
+
--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
 module.exports.index = async function(req, res){
     
@@ -22,28 +23,21 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-        if(post && post.user == req.user.id){
+        // if(post && post.user == req.user.id){
             post.remove();
     
             await Comment.deleteMany({post: req.params.id});
 
-            if(req.xhr){
-                console.log('xhr Gotten');
-                return res.status(200).json({
-                    data: {
-                        post_id: req.params.id
-                    },
-                    message: 'Post deleted successfully'
-                })
-            }
+            return res.json(200, {
+                message: "Post and associated comments are deleted"
+            });
 
-            req.flash('success', 'Post deleted successfully!');
-            return res.redirect('back');
-        }
-        else{
-            req.flash('error', 'You cannot delete this post!');
-        }
+            // req.flash('success', 'Post deleted successfully!');
+            
+        
     } catch(err) {
-        console.log('Err: ',err);
+        return res.json(500, {
+            message: "Internal Server error"
+        });
     }
 }
