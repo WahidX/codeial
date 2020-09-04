5f65b5d26ba2b5872e5368a30daed7ccb1c5ca30
WahidX <wahidx93@gmail.com>
2020-09-05 05:26:03 +0530

    JWT strategy complete

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -4,4 +4,21 @@ const ExtractJWT = require("passport-jwt").ExtractJwt;
 
 const User = require('../models/users');
 
-// Pending
+let opts = {
+	jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken,
+	secretOrKey: 'super-secret-key'
+}
+
+passport.use(new JWTstrategy(opts, function(payload, done){
+	User.findById(payload._id, function(err, user){
+		if(err){ console.log('Err: ', err); return; }
+
+		if(user){
+			return done(null, user);
+		}else{
+			done(null, false);
+		}
+	});
+}));
+
+module.exports = passport;
