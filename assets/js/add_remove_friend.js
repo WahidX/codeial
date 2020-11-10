4d3f485e0210853e9708ef72f22430db2c2799be
WahidX <wahidx93@gmail.com>
2020-11-10 22:08:14 +0530

    ajax handled for add-remove friend

--- assets/js/add_remove_friend.js
+++ assets/js/add_remove_friend.js
@@ -1,6 +1,33 @@
 {
-    addRemove = () => {
-        console.log("this: ",this);
+    let addRemoveFriend = function(){
+
+        let addRemoveBtn = $('#add-friend');
+        addRemoveBtn.on("click", function(e){
+            e.preventDefault();
+            
+            // Ajax call to friendship req
+            $.ajax({
+                type: 'POST',
+                url: $(addRemoveBtn).attr('href'),
+                success: function(data){
+                    console.log(data);
+                    if (data.data.friend === true){
+                        addRemoveBtn.attr('class', 'friend');
+                        addRemoveBtn.html("Remove friend");
+                    }
+                    else{
+                        addRemoveBtn.attr('class', 'non-friend');
+                        addRemoveBtn.html("Add friend");
+                    }
+                },
+                error: function(err){
+                    console.log("Err: ", err);
+                }
+            });
+
+
+        });
     };
 
+    addRemoveFriend();
 }
--- controllers/friendship_controller.js
+++ controllers/friendship_controller.js
@@ -1,14 +1,21 @@
-const { remove } = require('../models/friendship');
+const { model } = require('../models/friendship');
+const friendship = require('../models/friendship');
 const Like = require('../models/friendship');
 // const Post = require('../models/');
 // const Comment = require('../models/comment');
 
 module.exports.addRemove = async function(req, res){
     try{
-        let friend = false;
+        let friend = true;
 
         // friend/add-remove/?id1=123&id2=321
-        console.log("id1 = ",req.query.id1, "id2 = ",req.query.id1);        
+        console.log("id1 = ",req.query.id1, "id2 = ",req.query.id2);        
+
+        // TODO:
+        // check if there's any friendship exists
+        // add or remove friend accordingly
+        // also update friend array in users model
+        // send var friend true or false
 
         return res.json(200, {
             message: "Request successful",
