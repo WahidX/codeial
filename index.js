dff1744b0e57151fd3b4a6f950b3beb6c9941611
WahidX <wahidx93@gmail.com>
2020-12-19 01:53:55 +0530

    cors allowed

--- index.js
+++ index.js
@@ -79,6 +79,12 @@ app.use(passport.setAuthenticatedUser);
 app.use(flash());
 app.use(customMware.setFlash);
 
+// Allowing cors
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "Origin, X-requested-With, Content-Type, Accept");
+});
+
 // Router
 app.use('/', require('./routes'));
 
