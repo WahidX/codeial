2f01b0b0bc5d1fbf03e92f485258bab953bcf1c5
WahidX <wahidx93@gmail.com>
2020-12-19 02:03:23 +0530

    cors fix

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ app.use(customMware.setFlash);
 // Allowing cors
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Origin, X-requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // Router
