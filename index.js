f549d0974f18149f9ea47d97253effa25836e2ce
WahidX <wahidx93@gmail.com>
2020-12-22 21:17:55 +0530

    origin-methods allowed

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.use(customMware.setFlash);
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
   next();
 });
 
