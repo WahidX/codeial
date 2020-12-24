3e6df5c36a56ca3c01a6a5ef68449424382c934b
WahidX <wahidx93@gmail.com>
2020-12-25 00:31:11 +0530

    Xside pass hash enabled

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,58 +1,62 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/users');
 
-passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passReqToCallback: true,
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passReqToCallback: true,
     },
-    function(req, email, password, done){
-        User.findOne({email:email}, function(err, user){
-            if(err){
-                req.flash('error', 'email not registered');
-                return done(err);
-            }
-
-            if(!user || user.password != password){
-                req.flash('error', 'Invalid username/password');
-                return done(null, false);
-            }
-            return done(null, user);
-        });
-    }
-))
-
-
-passport.serializeUser(function(user, done){
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        if(err){
-            console.log("Err while getting user--> passport");
-            return done(err);
+    async function (req, email, password, done) {
+      User.findOne({ email: email }, async function (err, user) {
+        if (err) {
+          req.flash('error', 'email not registered');
+          return done(err);
         }
 
+        if (!user) {
+          let isMatch = await bcrypt.compare(password, user.password);
+          if (isMatch) {
+            req.flash('error', 'Invalid username/password');
+            return done(null, false);
+          }
+        }
         return done(null, user);
-    });
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-
-passport.checkAuthentication = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log('Err while getting user--> passport');
+      return done(err);
     }
-    return res.redirect('/user/login');
-}
 
-passport.setAuthenticatedUser = function(req, res, next){
-    if(req.isAuthenticated()){
-        res.locals.user = req.user;
-    }
-    next();
-}
+    return done(null, user);
+  });
+});
 
+passport.checkAuthentication = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect('/user/login');
+};
+
+passport.setAuthenticatedUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  next();
+};
 
 module.exports = passport;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,273 +4,291 @@ const path = require('path');
 const passport = require('passport');
 const crypto = require('crypto');
 const Reset_Token = require('../models/reset_token');
-const queue = require("../config/kue");
+const queue = require('../config/kue');
 const Friendship = require('../models/friendship');
-
+const bcrypt = require('bcrypt');
 
 module.exports = {
-    profile : async function(req, res) {
-        try{
-            let user = await User.findById(req.params.id);
+  profile: async function (req, res) {
+    try {
+      let user = await User.findById(req.params.id);
 
-            //check if user's friend or not
-            let match1, match2;
-            match1 = await Friendship.find({
-                from_user: req.user.id,
-                to_user: req.params.id
-            });
-            
-            if(match1.length === 0){
-                match2 = await Friendship.find({
-                    from_user: req.params.id,
-                    to_user: req.user.id
-                });
-            }
+      //check if user's friend or not
+      let match1, match2;
+      match1 = await Friendship.find({
+        from_user: req.user.id,
+        to_user: req.params.id,
+      });
 
-            let isFriend = false;
-            if(match1.length > 0 || match2.length > 0){
-                isFriend = true;
-            }
-            console.log('isFried: ', isFriend);
-    
-            return res.render('profile', {
-                title: 'Profile',
-                profile_user: user,
-                isFriend: isFriend
-            });
-        }
-        catch(err){
-            req.flash('error', 'Invalid Request');
-            console.log('Err : ', err);
-            return res.redirect('back');
-        }
-
-        
-    },
-
-    updateUser : async function(req, res){
-        if(req.user.id == req.params.id){
-            try{
-                let user = await User.findById(req.params.id);
-                User.uploadedAvatar(req, res, function(err){
-                    if(err){console.log("Err: ",err);return;}
-                    
-                    user.email = req.body.email;
-                    user.name = req.body.name;
-
-                    if(req.file){
-                        if(user.avatar){
-                            fs.unlinkSync(path.join(__dirname, '..', user.avatar));
-                        }
-
-                        user.avatar = User.avatarPath + '/' + req.file.filename;
-                    }
-                    user.save();
-                    return res.redirect('back');
-                });
-                
-
-            }catch(err) {
-                console.log('Err: ',err);
-                return;
-            }
-            
-        }
-        else{
-            req.flash('error', 'Unauthorized');
-            return res.status(401).send('Unauthourized');
-        }
-    },
-
-    login : function(req, res) {
-        if(req.isAuthenticated()){
-            return res.redirect('/user/profile/');
-        }
-
-        return res.render('login', {
-            title : 'Login'
+      if (match1.length === 0) {
+        match2 = await Friendship.find({
+          from_user: req.params.id,
+          to_user: req.user.id,
         });
+      }
 
-    },
+      let isFriend = false;
+      if (match1.length > 0 || match2.length > 0) {
+        isFriend = true;
+      }
+      console.log('isFried: ', isFriend);
 
-    signup : function(req, res) {
-        if(req.isAuthenticated()){
-            return res.redirect('/user/profile/');
-        }
+      return res.render('profile', {
+        title: 'Profile',
+        profile_user: user,
+        isFriend: isFriend,
+      });
+    } catch (err) {
+      req.flash('error', 'Invalid Request');
+      console.log('Err : ', err);
+      return res.redirect('back');
+    }
+  },
 
-        return res.render('signup', {
-            title : 'Signup'
-        });
-    },
-
-    createUser : function(req, res) {
-        
-        if(req.body.password != req.body.confirm_password){
-            req.flash('error',"Password doesn't match!");
-            console.log("password doesn't match");
-            return res.redirect('back');
-        }
-
-        User.findOne({email:req.body.email}, function(err, user){
-            if(err){console.log('Error in getting email');return;}
-
-            if(!user) {
-                User.create({
-                    email: req.body.email,
-                    password: req.body.password,
-                    name: req.body.name
-                }, function(err,newUser){
-                    if(err){console.log("Error in creating new user");return;}
-                    console.log('New user created');
-                    return res.redirect('/user/login');
-                })
-            }
-            else{
-                console.log('Email alreay exists');
-                return res.redirect('back');
-            }
-        })
-
-    },
-
-    createSession : function(req, res) {
-        req.flash('success', 'Logged in');
-            
-        return res.redirect('/');
-    },
-
-    destroySession: function(req, res) {
-        req.logout();
-        req.flash('success', 'Logged out');
-        return res.redirect('/');
-    },
-
-    // These are for reset and changing password
-    // reset password comes here
-
-    forgetPassword: function(req, res){
-        return res.render('confirm_email', {
-            title: 'Confirm Email'
-        });
-    },
-
-    checkPassword: function(req, res){
-        if (req.user.password !== req.body.old_password){
-            req.flash('error', 'Incorrect password!');
-            return res.redirect('back');
-        }
-
-        return res.redirect(`/user/send-reset-link/${req.user.id}`);
-    },
-
-    resetPassword: function(req, res){
-        return res.render('confirm_password', {
-            title: 'Confirm Password'
-        });
-    },
-
-    checkEmail: async function(req, res){
-        let user = await User.findOne({email:req.body.email});
-
-        if(!user){
-            req.flash('error', 'Email not registered!');
-            return res.redirect('back');
-        }
-        
-        return res.redirect(`/user/send-reset-link/${user.id}`);
-    },
-
-    sendResetLink: async function(req, res){
-        // check user id
+  updateUser: async function (req, res) {
+    if (req.user.id == req.params.id) {
+      try {
         let user = await User.findById(req.params.id);
-        if(!user){
-            req.flash('error', 'Invalid URL!');
-            return res.redirect('back');
-        }
+        User.uploadedAvatar(req, res, function (err) {
+          if (err) {
+            console.log('Err: ', err);
+            return;
+          }
 
-        // invalidating user's previous valid tokens
-        Reset_Token.updateMany(
-            {user: user._id, isvalid: "true"}, 
-            {$set: { isvalid: "false" }}, 
-            function(err){ console.log("Err: ",err); return; }
+          user.email = req.body.email;
+          user.name = req.body.name;
+
+          if (req.file) {
+            if (user.avatar) {
+              fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+            }
+
+            user.avatar = User.avatarPath + '/' + req.file.filename;
+          }
+          user.save();
+          return res.redirect('back');
+        });
+      } catch (err) {
+        console.log('Err: ', err);
+        return;
+      }
+    } else {
+      req.flash('error', 'Unauthorized');
+      return res.status(401).send('Unauthourized');
+    }
+  },
+
+  login: function (req, res) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/user/profile/');
+    }
+
+    return res.render('login', {
+      title: 'Login',
+    });
+  },
+
+  signup: function (req, res) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/user/profile/');
+    }
+
+    return res.render('signup', {
+      title: 'Signup',
+    });
+  },
+
+  createUser: async function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
+      req.flash('error', "Password doesn't match!");
+      console.log("password doesn't match");
+      return res.redirect('back');
+    }
+
+    User.findOne({ email: req.body.email }, async function (err, user) {
+      if (err) {
+        console.log('Error in getting email');
+        return;
+      }
+
+      if (!user) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        User.create(
+          {
+            email: req.body.email,
+            password: hashedPassword,
+            name: req.body.name,
+          },
+          function (err, newUser) {
+            if (err) {
+              console.log('Error in creating new user');
+              return;
+            }
+            console.log('New user created');
+            return res.redirect('/user/login');
+          }
         );
-
-        // Generating new token and storing it in DB
-        const token = crypto.randomBytes(20).toString('hex');
-
-        let resetToken = await Reset_Token.create({
-            user: user.id,
-            access_token: token,
-            isvalid: true
-        });
-        
-
-        resetToken = await resetToken.populate('user','_id name email').execPopulate();
-
-        let job = queue.create('resetmails', resetToken).save(function(err){
-            if(err){console.log("Err: ",err);return;}
-        });
-        // Mail sent and DB updated
-        req.flash('success', 'Reset link sent to mail');
-
-        // TODO: in EJS resend mail option
+      } else {
+        console.log('Email alreay exists');
         return res.redirect('back');
-    },
+      }
+    });
+  },
 
-    resetLinkCheck: async function(req, res){
+  createSession: function (req, res) {
+    req.flash('success', 'Logged in');
 
-        let user = await User.findById(req.params.id);
-        if (!user){
-            req.flash('error','Invalid Request');
-            return res.redirect('/');
-        }
-        let token = await Reset_Token.findOne({user:user.id, isvalid:'true'}, {}, { sort: { 'created_at' : -1 } });
+    return res.redirect('/');
+  },
 
-        console.log("Token: ",token);
-        if (token.access_token !== req.params.token){
-            req.flash('error', 'Invalid Request');
-            return res.redirect('/');
-        }
+  destroySession: function (req, res) {
+    req.logout();
+    req.flash('success', 'Logged out');
+    return res.redirect('/');
+  },
 
-        req.flash('success', 'Now Change Password');
+  // These are for reset and changing password
+  // reset password comes here
 
-        return res.render('update-password',{
-            title: 'New Password',
-            id: user._id
-        });
-    },
+  forgetPassword: function (req, res) {
+    return res.render('confirm_email', {
+      title: 'Confirm Email',
+    });
+  },
 
-    updatePassword: async function(req, res){
-        let user = await User.findById(req.params.id);
-        if (!user){
-            req.flash('error', 'Invalid request');
-            return res.redirect('back');
-        }
+  checkPassword: function (req, res) {
+    if (req.user.password !== req.body.old_password) {
+      req.flash('error', 'Incorrect password!');
+      return res.redirect('back');
+    }
 
-        if (req.body.new_password !== req.body.confirm_password){
-            req.flash('error', 'Passwords didn\'t match');
-            return res.redirect('back');
-        }
+    return res.redirect(`/user/send-reset-link/${req.user.id}`);
+  },
 
-        // Check if the user made the update request
-        let token = await Reset_Token.findOne({user: req.params.id, isvalid: true});
-        if (!token){
-            console.log('No reset_token is valid for ID ',req.params.id);
-            req.flash('error','Invalid request');
-        }
-        
-        token.isvalid = 'false';
-        token.save();
+  resetPassword: function (req, res) {
+    return res.render('confirm_password', {
+      title: 'Confirm Password',
+    });
+  },
 
-        user.password = req.body.new_password;
-        user.save();
+  checkEmail: async function (req, res) {
+    let user = await User.findOne({ email: req.body.email });
 
-        req.flash('success', 'Password changed Successfully');
-        if (req.isAuthenticated()){
-            req.logout();
-        }
-        return res.redirect('/user/login');
+    if (!user) {
+      req.flash('error', 'Email not registered!');
+      return res.redirect('back');
+    }
 
-    } 
- 
+    return res.redirect(`/user/send-reset-link/${user.id}`);
+  },
+
+  sendResetLink: async function (req, res) {
+    // check user id
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      req.flash('error', 'Invalid URL!');
+      return res.redirect('back');
+    }
+
+    // invalidating user's previous valid tokens
+    Reset_Token.updateMany(
+      { user: user._id, isvalid: 'true' },
+      { $set: { isvalid: 'false' } },
+      function (err) {
+        console.log('Err: ', err);
+        return;
+      }
+    );
+
+    // Generating new token and storing it in DB
+    const token = crypto.randomBytes(20).toString('hex');
+
+    let resetToken = await Reset_Token.create({
+      user: user.id,
+      access_token: token,
+      isvalid: true,
+    });
+
+    resetToken = await resetToken
+      .populate('user', '_id name email')
+      .execPopulate();
+
+    let job = queue.create('resetmails', resetToken).save(function (err) {
+      if (err) {
+        console.log('Err: ', err);
+        return;
+      }
+    });
+    // Mail sent and DB updated
+    req.flash('success', 'Reset link sent to mail');
+
+    // TODO: in EJS resend mail option
+    return res.redirect('back');
+  },
+
+  resetLinkCheck: async function (req, res) {
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      req.flash('error', 'Invalid Request');
+      return res.redirect('/');
+    }
+    let token = await Reset_Token.findOne(
+      { user: user.id, isvalid: 'true' },
+      {},
+      { sort: { created_at: -1 } }
+    );
+
+    console.log('Token: ', token);
+    if (token.access_token !== req.params.token) {
+      req.flash('error', 'Invalid Request');
+      return res.redirect('/');
+    }
+
+    req.flash('success', 'Now Change Password');
+
+    return res.render('update-password', {
+      title: 'New Password',
+      id: user._id,
+    });
+  },
+
+  updatePassword: async function (req, res) {
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      req.flash('error', 'Invalid request');
+      return res.redirect('back');
+    }
+
+    if (req.body.new_password !== req.body.confirm_password) {
+      req.flash('error', "Passwords didn't match");
+      return res.redirect('back');
+    }
+
+    // Check if the user made the update request
+    let token = await Reset_Token.findOne({
+      user: req.params.id,
+      isvalid: true,
+    });
+    if (!token) {
+      console.log('No reset_token is valid for ID ', req.params.id);
+      req.flash('error', 'Invalid request');
+    }
+
+    token.isvalid = 'false';
+    token.save();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    user.password = hashedPassword;
+    user.save();
+
+    req.flash('success', 'Password changed Successfully');
+    if (req.isAuthenticated()) {
+      req.logout();
+    }
+    return res.redirect('/user/login');
+  },
 };
