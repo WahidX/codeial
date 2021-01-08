14e9ad7445acee0aef724ed11d14a4b5066c1144
WahidX <wahidx93@gmail.com>
2021-01-09 03:45:24 +0530

    minor fixes

--- config/mongoose.js
+++ config/mongoose.js
@@ -15,17 +15,6 @@ db.on('error', console.error.bind(console, 'Error connecting mongodb'));
 
 db.once('open', function () {
   console.log('Connected to mongoDB');
-  console.log('MONGO: ', `mongodb://localhost/${env.db}`);
 });
 
 module.exports = db;
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri =
-//   'mongodb+srv://droidx:<password>@cluster0.dlecj.mongodb.net/<dbname>?retryWrites=true&w=majority';
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect((err) => {
-//   const collection = client.db('test').collection('devices');
-//   // perform actions on the collection object
-//   client.close();
-// });
--- index.js
+++ index.js
@@ -83,10 +83,23 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 // Allowing cors
-// app.use(function(req, res, next){
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, Authorization, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'POST, GET, OPTIONS, DELETE, PATCH'
+//   );
+//   next();
+// });
+
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   res.header('Access-Control-Allow-Methods', '*');
 //   next();
 // });
 
